e47bf04ce6efb3692b3ed3a297e657342ed190a3
nitu21042002 <yadavnitu564@gmail.com>
2026-07-12 22:48:06 +0530

    Add 02_Shallow_Copy_List.py

--- Python_Basic/02_Shallow_Copy_List.py
+++ Python_Basic/02_Shallow_Copy_List.py
@@ -4,9 +4,12 @@ lst2=lst1.copy()  #shallow copy of lst1
 
 lst1[0]=0 #change in liner list modify only original list not coped list due to 
          #shallow copy shallow copy allow to change only nested list not liner list
-
+print("lst1:",lst1)
 lst1[4][0]=0 #change in both list modifyed original and coped list  due to 
          #shallow copy shallow copy allow to change only nested list
-         
-print(lst1)
-print(lst2)
+print("lst2:",lst2)
+
+lst2[1]=9 #modify both list because changing  in liner list
+print("lst2:",lst2)
+lst2[4][2]=10 #modify only lst2 list because changing  in nested list
+print("lst2:",lst2)
---
+++ Python_Basic/03_Shallow_Copy_Nested_List.py
@@ -0,0 +1,1 @@
+#Write a program to demonstrate shallow copy of a nested list.
