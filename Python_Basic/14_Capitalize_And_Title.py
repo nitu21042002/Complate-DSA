a4ac6157984b1d153357348e04a45f1881728311
nitu21042002 <yadavnitu564@gmail.com>
2026-07-13 00:29:51 +0530

    Add python_basic.py

---
+++ Python_Basic/14_Capitalize_And_Title.py
@@ -0,0 +1,5 @@
+#Write a program using capitalize() and title().
+var="nitu yadav"
+print(var.capitalize()) #capitalize make only first string as uppercase.
+
+print(var.title()) #title make all words first lettter upparcase.
