216c696927ba5cce47e67f7a4891743b06e6db6a
nitu21042002 <yadavnitu564@gmail.com>
2026-07-13 00:01:19 +0530

    Add 02_Shallow_Copy_List.py

---
+++ Python_Basic/10_Lambda_Function_Type.py
@@ -0,0 +1,9 @@
+#Write a program to find the type of a lambda function.
+add = lambda a,b:a+b
+#variable = lambda var1,var2:expression
+print(add(10,20))
+print(type(add))
+
+print(type(lambda a,b:a**b))
+
+
