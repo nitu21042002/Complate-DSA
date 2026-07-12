3bfacf3f83fdcf80b4b20486a0651a4ef3ad95ef
nitu21042002 <yadavnitu564@gmail.com>
2026-07-12 23:44:48 +0530

    Add 02_Shallow_Copy_List.py

--- Python_Basic/07_Decorator.py
+++ Python_Basic/07_Decorator.py
@@ -1,1 +1,19 @@
-#Write a program to check the boolean value of different strings.
+#Write a program using a Decorator.
+def decorator(func):
+    def wrapper(*args,**kwargs):
+        print("Before Decorator!!")
+        res=func(*args, **kwargs)
+        print("After Decorator use!!")
+        return res
+    return wrapper
+
+
+@decorator
+def sum(*args):
+    total=0
+    for i in args:
+        total=total+i
+    print(total)
+
+sum(1,2,3,45,12,85)
+
