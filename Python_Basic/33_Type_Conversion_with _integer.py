0d4cc750e2ea820e5faffc6af382434e46c35749
nitu21042002 <yadavnitu564@gmail.com>
2026-07-15 15:19:43 +0530

    Add python_basic.py

---
+++ Python_Basic/33_Type_Conversion_with _integer.py
@@ -0,0 +1,17 @@
+# Write a program to convert strings to integers and integers to strings.
+
+# String to Integer
+str_num = "100"
+int_num = int(str_num)
+
+print("String:", str_num, type(str_num))
+print("Integer:", int_num, type(int_num))
+
+print()
+
+# Integer to String
+num = 200
+str_value = str(num)
+
+print("Integer:", num, type(num))
+print("String:", str_value, type(str_value))
